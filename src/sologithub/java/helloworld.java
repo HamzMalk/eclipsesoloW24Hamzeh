4dc2cb2aa254bee9e1f947062264653b1260b397
HamzMalk <melkawihz@gmail.com>
2024-02-12 20:59:08 -0500

    third commit with alternative

--- src/sologithub/java/helloworld.java
+++ src/sologithub/java/helloworld.java
@@ -4,6 +4,7 @@ public class helloworld {
 
 public static void main (String[] args) {
 	System.out.println("Hello, GitWorld!");
-	System.out.println("Hello, again.")
+	System.out.println("Hello, again.");
+	System.out.println("Alternate");
 }
 }
