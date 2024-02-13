73eeec0b4a5dca202bb6f19b17aad4942a6afa6d
HamzMalk <melkawihz@gmail.com>
2024-02-13 18:03:54 -0500

    main commit

--- src/sologithub/java/helloworld.java
+++ src/sologithub/java/helloworld.java
@@ -5,6 +5,6 @@ public class helloworld {
 public static void main (String[] args) {
 	System.out.println("Hello, GitWorld Im in the alternate!");
 	System.out.println("Hello, again.");
-	System.out.println("Alternate");
+	System.out.println("Alternate back to main");
 }
 }
