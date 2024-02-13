2979ebe647cb4d1f606f4e0ac729a588cf281114
HamzMalk <melkawihz@gmail.com>
2024-02-13 17:57:55 -0500

    ALTERNATIVE COMMIT

--- src/sologithub/java/helloworld.java
+++ src/sologithub/java/helloworld.java
@@ -3,7 +3,7 @@ package sologithub.java;
 public class helloworld {
 
 public static void main (String[] args) {
-	System.out.println("Hello, GitWorld!");
+	System.out.println("Hello, GitWorld Im in the alternate!");
 	System.out.println("Hello, again.");
 	System.out.println("Alternate");
 }
